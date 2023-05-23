37e26959acde7f8700231b3ef3ddffab5894f0b7
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-23 18:48:26 +0200

    add all script of javaScript file

---
+++ script.js
@@ -0,0 +1,73 @@
+//=========== Déclaration des Varialbes ===========\\
+// let images =[
+//     "dice-1.png",
+//     "dice-2.png",
+//     "dice-3.png",
+//     "dice-4.png",
+//     "dice-5.png",
+//     "dice-6.png"
+// ];
+
+let scores, 
+    roundScore,
+    activePlayer,
+    gamePlaying ;
+
+
+    //========================= déclaration des fonctions =========================
+    init();
+    function init() {
+        scores = [0, 0];
+        roundScore = 0;
+        activePlayer = 0;
+        gamePlaying = true ;
+        document.getElementById("player1Round").textContent = "0";
+        document.getElementById("player1Global").textContent = "0";
+        document.getElementById("player2Round").textContent = "0";
+        document.getElementById("player2Global").textContent = "0";        
+
+        document.getElementById("rollBtn").disabled= false;    
+        document.getElementById("holdBtn").disabled= false;     
+    }
+    //=========================== rollDice function building ======================
+
+    function rollDice() {
+        if (gamePlaying) {
+            let dice = Math.floor(Math.random() * 6) + 1 ;
+            let diceDOM = document.getElementById("die-1");
+            diceDOM.src = "dice-" + dice + ".png";
+
+            if(dice !== 1) {
+                roundScore += dice;
+                document.getElementById("player" +(activePlayer + 1) + "Round").textContent = roundScore;
+            }else{
+                nextPlayer();
+            }
+        }
+    }
+
+        //=========================== hold function building ========================
+        function hold() {
+            if (gamePlaying) {
+                scores[activePlayer] += roundScore;
+                document.getElementById("player" + (activePlayer + 1) + "Global").textContent = scores[activePlayer];
+
+                if (scores[activePlayer] >= 100) {
+                    document.getElementById("player" + (activePlayer + 1)).style.backgroundColor = "green";
+                    document.getElementById("player" + (activePlayer + 1)).style.borderRadius = "15" + "px";
+                    document.getElementsByTagName("h2" + (activePlayer + 1)).style.color = "yellow";
+                    document.getElementById("rollBtn").disabled = true ;
+                    document.getElementById("holdBtn").disabled = true ;
+                    gamePlaying = false;
+                }else{
+                    nextPlayer();
+
+                }
+            }
+        }
+    
+        function nextPlayer() {
+            roundScore = 0;
+            document.getElementById("player" + (activePlayer + 1) + "Round").textContent = "0";
+            activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+         }
